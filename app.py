fc357404be7cdc3252ce1b92287f79e070493c9b
Abdelbar Terbachi <abdelbarterbachi@gmail.com>
2023-11-18 15:44:19 +0100

    second commit

--- app.py
+++ app.py
@@ -1,34 +1,77 @@
 # app.py
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
+from flask_cors import CORS
+from flask_socketio import SocketIO
 from pyspark.ml import PipelineModel
+from confluent_kafka import Consumer, KafkaException, KafkaError
+import json
+import threading
 
 app = Flask(__name__)
+CORS(app)
+socketio = SocketIO(app)
 
 # Load the Spark ML model
 model_path = "models/churn_prediction_model"
 spark_ml_model = PipelineModel.load(model_path)
 
+# Kafka configuration
+bootstrap_servers = 'localhost:9092'
+topic_name = 'customer_churn'
 
-@app.route('/predict', methods=['POST'])
-def predict():
+
+# Function to consume data from Kafka topic and predict in real-time
+def consume_from_kafka_streams():
+    consumer_conf = {
+        'bootstrap.servers': bootstrap_servers,
+        'group.id': 'my_consumer_group',
+        'auto.offset.reset': 'earliest'  # Start reading from the beginning of the topic
+    }
+
+    consumer = Consumer(consumer_conf)
+    consumer.subscribe([topic_name])
+
     try:
-        # Get input data from the request
-        data = request.get_json()
+        while True:
+            msg = consumer.poll(1.0)
 
-        # Assuming your input data is in the format expected by your model
-        features = data['features']
+            if msg is None:
+                continue
 
-        # Make predictions using the loaded Spark ML model
-        predictions = spark_ml_model.transform(features)
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    continue
+                elif msg.error():
+                    raise KafkaException(msg.error())
 
-        # Extract the prediction result (adjust this based on your model output)
-        prediction_result = predictions.select("prediction").collect()[0]["prediction"]
+            # Process the consumed message (print it in this example)
+            consumed_data = json.loads(msg.value().decode('utf-8'))
 
-        return jsonify({'prediction': prediction_result})
+            # Make predictions using the loaded Spark ML model
+            predictions = spark_ml_model.transform([consumed_data])
 
-    except Exception as e:
-        return jsonify({'error': str(e)})
+            # Extract the prediction result
+            prediction_result = predictions.select("prediction").collect()[0]["prediction"]
+
+            # Send prediction to the frontend via Socket.IO
+            socketio.emit('prediction_response', {'prediction': prediction_result})
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        consumer.close()
+
+
+# Start Kafka data consumption and prediction in a separate thread
+kafka_thread = threading.Thread(target=consume_from_kafka_streams)
+kafka_thread.start()
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
